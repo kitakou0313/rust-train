d8f6b98c458bb3af7596055b97b6a192ed0647f9
kitakou0313 <kitakou0313@gmail.com>
2024-03-07 14:13:53 +0000

    add: implement fib

--- comprehensive-rust/day1/exercise/src/main.rs
+++ comprehensive-rust/day1/exercise/src/main.rs
@@ -1,17 +1,12 @@
-fn take_u32(i: u32) {
-    println!("{} is u32", i)
-}
+fn fib(n: u32) -> u32 {
+    if n == 0 || n == 1 {
+        return 1;
+    }
 
-fn take_i8(i: i8) {
-    println!("{} is i8", i)
+    return fib(n-1) + fib(n-2);
 }
 
 fn main() {
-    // 型を指定しない場合，関数への渡され方，参照され方から型を推論する
     let x = 10;
-    let y = 2;
-
-    take_u32(x);
-    take_u32(y);
-    take_i8(y);
+    println!("fib:{}", fib(x))
 }

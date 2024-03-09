0458db43756350c710472ba84843dbb74a29c20b
kitakou0313 <kitakou0313@gmail.com>
2024-03-09 00:01:05 +0000

    add: add sample of break with label

--- comprehensive-rust/day1/exercise/src/main.rs
+++ comprehensive-rust/day1/exercise/src/main.rs
@@ -9,6 +9,23 @@ fn forloop() {
     }
 }
 
+fn nestedLoop() {
+    let map = [[5,4,3],[3,2,1]];
+    let trg = 3;
+    let mut num_of_scanned_elems = 0;
+
+    'outer: for i in 0..2 {
+        for j in 0..3 {
+            num_of_scanned_elems += 1;
+            if map[i][j] == trg {
+                break 'outer;
+            }
+        }
+    }
+
+    println!("{}", num_of_scanned_elems)
+}
+
 fn main() {
-    forloop()
+    nestedLoop()
 }

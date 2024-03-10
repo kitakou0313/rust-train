6f305aaec63903a1daeba147d56f755768b4f69c
kitakou0313 <kitakou0313@gmail.com>
2024-03-10 13:33:47 +0000

    add:add sample of block

--- comprehensive-rust/day1/exercise/src/main.rs
+++ comprehensive-rust/day1/exercise/src/main.rs
@@ -1,31 +1,9 @@
-fn forloop() {
-    for elem in 1..5 {
-        println!("{}", elem)
-    }
-
-    
-    for elem in 1..=5 {
-        println!("{}", elem)
-    }
-}
-
-fn nestedLoop() {
-    let map = [[5,4,3],[3,2,1]];
-    let trg = 3;
-    let mut num_of_scanned_elems = 0;
-
-    'outer: for i in 0..2 {
-        for j in 0..3 {
-            num_of_scanned_elems += 1;
-            if map[i][j] == trg {
-                break 'outer;
-            }
-        }
-    }
-
-    println!("{}", num_of_scanned_elems)
-}
-
 fn main() {
-    nestedLoop()
+    let y = 3;
+    let x = {
+        let z = 10;
+        y - z
+    };
+
+    println!("{}", x)
 }

9f8d6ac4778b50da18675be7ebd1bd25754fe001
kitakou0313 <kitakou0313@gmail.com>
2024-03-12 14:37:44 +0000

    add: add sample of function in Rust

--- comprehensive-rust/day1/exercise/src/main.rs
+++ comprehensive-rust/day1/exercise/src/main.rs
@@ -1,14 +1,13 @@
-fn main() {
-    let a = 10;
-
-    {
-        let a = 20;
-        println!("{}", a);
-
-        // shadowing
-        let a = true;
-        println!("{}", a)
+fn gcd(a: u32, b:u32) -> u32 {
+    // 最後のexpressionが自動的に返り値になる
+    // ;をつけると返らない
+    if b > 0 {
+        {gcd(b, a % b)}
+    }else {
+        a
     }
+}
 
-    println!("{}", a)
+fn main() {
+    println!("{}", gcd(101, 10))
 }

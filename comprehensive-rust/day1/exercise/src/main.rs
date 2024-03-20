cbc3780462367e7c0ebbeba6999649e3a99dae44
kitakou0313 <kitakou0313@gmail.com>
2024-03-20 01:28:54 +0000

    add: add sample of struct

--- comprehensive-rust/day1/exercise/src/main.rs
+++ comprehensive-rust/day1/exercise/src/main.rs
@@ -1,31 +1,17 @@
-fn traspose_matrix(matrix: [[i32;3];3]) -> [[i32;3];3] {
-    
-    let mut transposed_matrix: [[i32;3];3] = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-    ];
+struct Person{
+    name: String,
+    age: i32
+}
 
-    for i in 0..matrix.len(){
-        for j in 0..matrix[0].len() {
-            transposed_matrix[j][i] = matrix[i][j]
-        }
-    };
-
-    return  transposed_matrix;
+fn describe(person: &Person) {
+    println!("{} is {} years old.", person.name, person.age)
 }
 
 fn main() {
-    let matrix = [
-        [101, 102, 103], // <-- the comment makes rustfmt add a newline
-        [201, 202, 203],
-        [301, 302, 303],
-    ];
+    let peter = Person{
+        name: String::from("Peter"),
+        age: 27
+    };
 
-    println!("matrix: {:#?}", matrix);
-
-    let trasposed = traspose_matrix(matrix);
-    
-    println!("matrix: {:#?}", trasposed);
-
+    describe(&peter)
 }
